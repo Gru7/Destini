73ed17330887db3dac5f06a21c95f8f2132f8a2b
Gru7 <50362514+Gru7@users.noreply.github.com>
2021-05-07 17:31:41 +0300

    Add files via upload

---
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    
+    
+    var storyBrain = StoryBrain()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoise = sender.currentTitle!
+        
+         storyBrain.nextStory(userChoise)
+        
+           
+        
+        
+        updateUi()
+        
+        
+    }
+    
+    func updateUi(){
+        storyLabel.text=storyBrain.storries[storyBrain.storyNumber].title
+        choice1Button.setTitle(storyBrain.storries[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.storries[storyBrain.storyNumber].choice2, for: .normal)
+    }
+    
+    
+    
+    
+}
+
